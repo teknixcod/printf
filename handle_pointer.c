b689fb16afa9edec3f43ff13a9563bb96a0bb6af
teknixcod <Agboola_ayokanmi>
2023-09-22 11:13:26 -0700

    pointer test

--- handle_pointer.c
+++ handle_pointer.c
@@ -1,20 +1,31 @@
 #include "main.h"
 
 /**
- *pointer_handler - Handles the 'p' conversion specifier
- *@args: The va_list containing the argument (a pointer)
+ * pointer_handler - Handles the %p conversion specifier
+ * @args: The va_list containing the argument to be printed (a pointer)
  *
- *Return: The number of characters printed
+ * Return: The number of characters printed
  */
+
+#include "main.h"
+
 int pointer_handler(va_list args)
 {
-	unsigned long int ptr = (unsigned long int)va_arg(args, void *);
+	unsigned long int address;
+	char *hex_address;
+	int printed_chars = 0;
 
-	if (ptr == 0)
-		return (_printf("(nil)"));
+	address = (unsigned long int)va_arg(args, void *);
 
-	_putchar('0');
-	_putchar('x');
-	return (hexadecimal_handler(ptr) + 2);
+	hex_address = convert(address, 16, 1);
+
+	if (!hex_address)
+		return (-1);
+
+	printed_chars += _puts("0x");
+	printed_chars += _puts(hex_address);
+
+	free(hex_address);
+
+	return (printed_chars);
 }
-
