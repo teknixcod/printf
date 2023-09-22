d6f4e5e37ddd42a0dfe0400dd1ac831f76c141b8
teknixcod <Agboola_ayokanmi>
2023-09-22 10:24:20 -0700

    String test

--- handler_String.c
+++ handler_String.c
@@ -1,70 +1,34 @@
 #include "main.h"
 
 /**
- * String_handler - print exclusuives string.
- * @l: argument.
- * Return: the length of the string.
+ *String_handler - Handles the custom 'S' specifier
+ * @args: The va_list containing the argument
+ *
+ * Return: The number of characters printed
  */
-
-int String_handler(va_list l)
+int String_handler(va_list args)
 {
-	register short len = 0;
-	char *res, *s = va_arg(l, char *);
-	int count;
+	int i, count = 0;
+	char *str = va_arg(args, char *);
 
-	if (!s)
-		return (_puts(NULL_STRING));
-	for (; *s; s++)
+	if (str == NULL)
+		str = NULL_STRING;
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (isNonAlphaNumeric(*s))
+		if (str[i] < 32 || str[i] >= 127)
 		{
-			count += _puts("\\x");
-			res = convert(*s, 16, 0);
-			if (!res[1])
-				len += _putchar('0');
-			len += _puts(res);
+			_putchar('\\');
+			_putchar('x');
+			count += 2;
+			count += _printf("%02X", str[i]);
 		}
 		else
-			len += _putchar(*s);
+		{
+			_putchar(str[i]);
+			count++;
+		}
 	}
-	return (len);
-}
 
-/**
- * isNonAlphaNumeric - determines if char is a non-
- * alphanumeric char on ASCII table
- * @c: input char
- * Return: true or false
- */
-
-int isNonAlphaNumeric(char c)
-{
-	return ((c > 0 && c < 32) || c >= 127);
-}
-
-/**
- * convert - converts number and base into string
- * @num: input number
- * @base: input base
- * @lowercase: flag if hexa values need to be lowercase
- * Return: result string
- */
-
-char *convert(unsigned long int num, int base, int lowercase)
-{
-	static char *rep;
-	static char buffer[50];
-	char *ptr;
-
-	rep = (lowercase)
-		? "0123456789abcdef"
-		: "0123456789ABCDEF";
-	ptr = &buffer[49];
-	*ptr = NUL;
-	do {
-		*--ptr = rep[num % base];
-		num /= base;
-	} while (num);
-
-	return (ptr);
+	return (count);
 }
